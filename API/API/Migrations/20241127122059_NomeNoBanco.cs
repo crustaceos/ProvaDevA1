8d62602061166fe6b31248deb8b5dd6877f7a75e
Auto Logon <autologon@CSED.LAB>
2024-11-27 11:46:35 -0300

    ProvaFinalizada

---
+++ API/API/Migrations/20241127122059_NomeNoBanco.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class NomeNoBanco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "CategoriaId",
+                keyValue: "39be53a2-fc09-4b6a-bafa-18a6a23c8f6e",
+                column: "CriadoEm",
+                value: new DateTime(2024, 11, 30, 9, 20, 59, 194, DateTimeKind.Local).AddTicks(3614));
+
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "CategoriaId",
+                keyValue: "6d091456-5a2f-4b5a-98fc-f1a3b50a627d",
+                column: "CriadoEm",
+                value: new DateTime(2024, 11, 29, 9, 20, 59, 194, DateTimeKind.Local).AddTicks(3612));
+
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "CategoriaId",
+                keyValue: "bfe4e7dc-81e4-4e47-a67b-d4fbf3e124bd",
+                column: "CriadoEm",
+                value: new DateTime(2024, 11, 28, 9, 20, 59, 194, DateTimeKind.Local).AddTicks(3597));
+
+            migrationBuilder.UpdateData(
+                table: "Tarefas",
+                keyColumn: "TarefaId",
+                keyValue: "2f1b7dc1-3b9a-4e1a-a389-7f5d2f1c8f3e",
+                column: "CriadoEm",
+                value: new DateTime(2024, 11, 30, 9, 20, 59, 194, DateTimeKind.Local).AddTicks(3779));
+
+            migrationBuilder.UpdateData(
+                table: "Tarefas",
+                keyColumn: "TarefaId",
+                keyValue: "6a8b3e4d-5e4e-4f7e-bdc9-9181e456ad0e",
+                column: "CriadoEm",
+                value: new DateTime(2024, 12, 4, 9, 20, 59, 194, DateTimeKind.Local).AddTicks(3774));
+
+            migrationBuilder.UpdateData(
+                table: "Tarefas",
+                keyColumn: "TarefaId",
+                keyValue: "e5d4a7b9-1f9e-4c4a-ae3b-5b7c1a9d2e3f",
+                column: "CriadoEm",
+                value: new DateTime(2024, 12, 11, 9, 20, 59, 194, DateTimeKind.Local).AddTicks(3781));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "CategoriaId",
+                keyValue: "39be53a2-fc09-4b6a-bafa-18a6a23c8f6e",
+                column: "CriadoEm",
+                value: new DateTime(2024, 11, 30, 9, 15, 19, 394, DateTimeKind.Local).AddTicks(2067));
+
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "CategoriaId",
+                keyValue: "6d091456-5a2f-4b5a-98fc-f1a3b50a627d",
+                column: "CriadoEm",
+                value: new DateTime(2024, 11, 29, 9, 15, 19, 394, DateTimeKind.Local).AddTicks(2065));
+
+            migrationBuilder.UpdateData(
+                table: "Categorias",
+                keyColumn: "CategoriaId",
+                keyValue: "bfe4e7dc-81e4-4e47-a67b-d4fbf3e124bd",
+                column: "CriadoEm",
+                value: new DateTime(2024, 11, 28, 9, 15, 19, 394, DateTimeKind.Local).AddTicks(2051));
+
+            migrationBuilder.UpdateData(
+                table: "Tarefas",
+                keyColumn: "TarefaId",
+                keyValue: "2f1b7dc1-3b9a-4e1a-a389-7f5d2f1c8f3e",
+                column: "CriadoEm",
+                value: new DateTime(2024, 11, 30, 9, 15, 19, 394, DateTimeKind.Local).AddTicks(2184));
+
+            migrationBuilder.UpdateData(
+                table: "Tarefas",
+                keyColumn: "TarefaId",
+                keyValue: "6a8b3e4d-5e4e-4f7e-bdc9-9181e456ad0e",
+                column: "CriadoEm",
+                value: new DateTime(2024, 12, 4, 9, 15, 19, 394, DateTimeKind.Local).AddTicks(2176));
+
+            migrationBuilder.UpdateData(
+                table: "Tarefas",
+                keyColumn: "TarefaId",
+                keyValue: "e5d4a7b9-1f9e-4c4a-ae3b-5b7c1a9d2e3f",
+                column: "CriadoEm",
+                value: new DateTime(2024, 12, 11, 9, 15, 19, 394, DateTimeKind.Local).AddTicks(2186));
+        }
+    }
+}
---
+++ API/API/Program.cs
@@ -0,0 +1,114 @@
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDataContext>();
+
+builder.Services.AddCors(options =>
+    options.AddPolicy("Acesso Total",
+        configs => configs
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod())
+);
+
+
+var app = builder.Build();
+
+
+app.MapGet("/", () => "Prova A1");
+
+//ENDPOINTS DE CATEGORIA
+//GET: http://localhost:5273/api/categoria/listar
+app.MapGet("/api/categoria/listar", ([FromServices] AppDataContext ctx) =>
+{
+    if (ctx.Categorias.Any())
+    {
+        return Results.Ok(ctx.Categorias.ToList());
+    }
+    return Results.NotFound("Nenhuma categoria encontrada");
+});
+
+//POST: http://localhost:5273/api/categoria/cadastrar
+app.MapPost("/api/categoria/cadastrar", ([FromServices] AppDataContext ctx, [FromBody] Categoria categoria) =>
+{
+    ctx.Categorias.Add(categoria);
+    ctx.SaveChanges();
+    return Results.Created("", categoria);
+});
+
+//ENDPOINTS DE TAREFA
+//GET: http://localhost:5273/api/tarefas/listar
+app.MapGet("/api/tarefas/listar", ([FromServices] AppDataContext ctx) =>
+{
+    if (ctx.Tarefas.Any())
+    {
+        return Results.Ok(ctx.Tarefas.Include(x => x.Categoria).ToList());
+    }
+    return Results.NotFound("Nenhuma tarefa encontrada");
+});
+
+//POST: http://localhost:5273/api/tarefas/cadastrar
+app.MapPost("/api/tarefas/cadastrar", ([FromServices] AppDataContext ctx, [FromBody] Tarefa tarefa) =>
+{
+    Categoria? categoria = ctx.Categorias.Find(tarefa.CategoriaId);
+    if (categoria == null)
+    {
+        return Results.NotFound("Categoria não encontrada");
+    }
+    tarefa.Categoria = categoria;
+    ctx.Tarefas.Add(tarefa);
+    ctx.SaveChanges();
+    return Results.Created("", tarefa);
+});
+
+//PUT: http://localhost:5273/tarefas/alterar/{id}
+app.MapPut("/api/tarefas/alterar/{id}", ([FromServices] AppDataContext ctx, [FromRoute] string id, [FromBody] Tarefa tarefaAtualizada) =>
+{
+    Tarefa? tarefa = ctx.Tarefas.FirstOrDefault(x => x.TarefaId == id);
+
+    if (tarefa == null) return Results.NotFound("Tarefa não encontrada");
+
+    if (!string.IsNullOrEmpty(tarefaAtualizada.Status)) tarefa.Status = tarefaAtualizada.Status;
+    
+    ctx.Tarefas.Update(tarefa);
+    ctx.SaveChanges();
+
+    return Results.Ok("Tarefa atualizada com sucesso");
+});
+
+//GET: http://localhost:5273/tarefas/naoconcluidas
+app.MapGet("/api/tarefas/naoconcluidas", ([FromServices] AppDataContext ctx) =>
+{
+    if (ctx.Tarefas.Any())
+    {
+        return Results.Ok(
+            ctx.Tarefas
+                .Include(c => c.Categoria)
+                .Where(s => s.Status != "Concluido")
+                .ToList()
+            );
+    }
+    return Results.NotFound("Nenhuma Tarefa Concluída");
+});
+
+//GET: http://localhost:5273/tarefas/concluidas
+app.MapGet("/api/tarefas/concluidas", ([FromServices] AppDataContext ctx) =>
+{
+    if (ctx.Tarefas.Any())
+    {
+        return Results.Ok(
+            ctx.Tarefas
+                .Include(c => c.Categoria)
+                .Where(s => s.Status == "Concluida")
+                .ToList()
+        );
+    }
+    return Results.NotFound("Nenhuma tarefa conclída encontrada");
+});
+
+
+
+app.UseCors("Acesso Total");
+app.Run();
